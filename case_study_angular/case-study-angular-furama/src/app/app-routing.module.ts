c556ae366ad0f80c7d5612a72ec5e9c79e4e41a0
Your Name <you@example.com>
2023-03-08 15:49:36 +0700

    update case

--- case_study_angular/case-study-angular-furama/src/app/app-routing.module.ts
+++ case_study_angular/case-study-angular-furama/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CustomerComponent} from "./compoment/common/customer/customer.component";
+import {HomeComponent} from "./compoment/common/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: HomeComponent
+  },
+  {path: 'customer/list', component: CustomerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
