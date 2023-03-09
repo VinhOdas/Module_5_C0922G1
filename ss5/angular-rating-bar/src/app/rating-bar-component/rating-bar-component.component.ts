71df8b7af84b0e50acf0cedfc18b5221f3890e47
Your Name <you@example.com>
2023-03-09 09:40:56 +0700

    ss5 ratting bar

---
+++ ss5/angular-rating-bar/src/app/rating-bar-component/rating-bar-component.component.ts
@@ -0,0 +1,17 @@
+// @ts-ignore
+import { Component, OnInit } from '@angular/core';
+
+// @ts-ignore
+@Component({
+  selector: 'app-rating-bar-component',
+  templateUrl: './rating-bar-component.component.html',
+  styleUrls: ['./rating-bar-component.component.css']
+})
+export class RatingBarComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
